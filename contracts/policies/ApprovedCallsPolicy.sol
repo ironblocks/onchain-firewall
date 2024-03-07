90cd9b04f7d7ac481f1e5df1a80d0d0499e97ec3
davidberiro <deebenchimol@gmail.com>
2024-03-07 11:04:39 -0800

    styling

--- contracts/policies/ApprovedCallsPolicy.sol
+++ contracts/policies/ApprovedCallsPolicy.sol
@@ -3,7 +3,7 @@
 // Copyright (c) Ironblocks 2023
 pragma solidity 0.8.19;
 
-import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./FirewallPolicyBase.sol";
 
 /**
