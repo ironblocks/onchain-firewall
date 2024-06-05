88787b57f49c7f97e4b25e7e549a8a35b0000aac
Uri Chandler <uri@ironblocks.com>
2024-06-06 01:10:37 +0300

    chore: added firewall-policy package

---
+++ packages/firewall-policy/contracts/FirewallPolicyBase.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+// See LICENSE file for full license text.
+// Copyright (c) Ironblocks 2024
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./interfaces/IFirewallPolicy.sol";
+
+abstract contract FirewallPolicyBase is IFirewallPolicy, AccessControl {
+    bytes32 public constant POLICY_ADMIN_ROLE = keccak256("POLICY_ADMIN_ROLE");
+
+    mapping (address => bool) public authorizedExecutors;
+    mapping (address => bool) public approvedConsumer;
+
+    modifier isAuthorized(address consumer) {
+        require(authorizedExecutors[msg.sender], "FirewallPolicy: Only authorized executor");
+        require(approvedConsumer[consumer], "FirewallPolicy: Only approved consumers");
+        _;
+    }
+
+    constructor() {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function setConsumersStatuses(address[] calldata consumers, bool[] calldata statuses) external onlyRole(POLICY_ADMIN_ROLE) {
+        for (uint i = 0; i < consumers.length; i++) {
+            approvedConsumer[consumers[i]] = statuses[i];
+        }
+    }
+
+    function setExecutorStatus(address caller, bool status) external onlyRole(POLICY_ADMIN_ROLE) {
+        authorizedExecutors[caller] = status;
+    }
+}
