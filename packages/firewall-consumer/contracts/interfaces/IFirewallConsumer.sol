1d11526487b6cad4cf6622f29008cbd7863b5e4e
Uri Chandler <uri@ironblocks.com>
2024-06-06 01:01:43 +0300

    chore: added firewall-consumer package

---
+++ packages/firewall-consumer/contracts/interfaces/IFirewallConsumer.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+// See LICENSE file for full license text.
+// Copyright (c) Ironblocks 2024
+pragma solidity ^0.8;
+
+interface IFirewallConsumer {
+    function firewallAdmin() external returns (address);
+}
