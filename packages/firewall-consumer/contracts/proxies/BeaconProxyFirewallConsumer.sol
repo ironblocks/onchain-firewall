03325a27eadd0dbc1f4fb18c86edba387744fb91
Uri Chandler <uri@ironblocks.com>
2024-06-20 18:44:34 +0300

    docs: better comments for memory slots

--- packages/firewall-consumer/contracts/proxies/BeaconProxyFirewallConsumer.sol
+++ packages/firewall-consumer/contracts/proxies/BeaconProxyFirewallConsumer.sol
@@ -11,7 +11,8 @@ import {ProxyFirewallConsumerBase} from "./ProxyFirewallConsumerBase.sol";
  * with a zero-address in the constructor or if the contract is upgradeable and the proxy was initialized before this implementation was deployed
  */
 contract BeaconProxyFirewallConsumer is ProxyFirewallConsumerBase {
-    bytes32 private constant BEACON_SLOT = bytes32(uint256(keccak256("eip1967.proxy.beacon")) - 1);
+    // This is the keccak-256 hash of "eip1967.proxy.beacon" subtracted by 1
+    bytes32 private constant BEACON_SLOT = 0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50;
 
     /**
      * @dev Beacon Proxy Owner only function, allows the Beacon Proxy Owner to initialize the firewall admin in the following cases:
